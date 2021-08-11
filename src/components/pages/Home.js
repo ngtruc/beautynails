beb52be1268328ec67464c56beca5f23de3a6541
truc nguyen <>
2021-08-11 15:06:24 +0200

    bild decreased

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,6 @@ import React , {useState} from 'react';
 import '../../App.css';
 //import './HeroSection.css';
 import HeroSection from '../heroSection/HeroSection'
-import homepic from './homepic.jpg'
 import Cards from '../cards/Cards'
 
 function Home() {
@@ -11,10 +10,6 @@ function Home() {
   return (
     <>
     <HeroSection 
-      pic={homepic}
-      link='/angebot-preise'
-      name='Angebot und Preise'
-      text="Bei uns gibt es nur professionelles Nageldesign"
     />
     <Cards />
     </>
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,9 +1,6 @@
 import React , {useState} from 'react';
 import '../../App.css';
 //import './HeroSection.css';
-
-import homepic from './homepic.jpg'
-import Cards from '../cards/Cards'
 import PreisList from '../services/PreisList'
 
 
